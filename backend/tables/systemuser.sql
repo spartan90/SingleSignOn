ba81210299d66996b0c16cec0d127a539c23b6d9
sarangmane607 <62138683+sarangmane607@users.noreply.github.com>
2020-04-26 20:47:35 +0530

    formatted
    formatted

--- backend/tables/systemuser.sql
+++ backend/tables/systemuser.sql
@@ -1,6 +1,6 @@
 create table systemuser (
 	USER_SEQ_NO number(20) not null,
-    USER_NAME VARCHAR2(200),
+	USER_NAME VARCHAR2(200),
 	PASSWORD VARCHAR2(100) not null,
 	EMAIL_ID VARCHAR2(100),
 	MOBILE_NO VARCHAR2(12)

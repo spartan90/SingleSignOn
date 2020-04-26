72643c0fe2e2dd65c94de63092d6ee50954878af
sarangmane607 <62138683+sarangmane607@users.noreply.github.com>
2020-04-26 22:19:38 +0530

    private data
    private data removed

--- backend/populatedata/basicdata.sql
+++ backend/populatedata/basicdata.sql
@@ -1,3 +1,3 @@
-insert into systemuser(USER_SEQ_NO, USER_NAME, PASSWORD, EMAIL_ID, MOBILE_NO) values(1, 'Sarang Mane', '$2a$10$IsII2xD8FpRREiOLz/bCle5Ape/Eq6EwCyXoPhlbGOULJvZ5Ygh2.', 'sarang.mane.607@gmail.com', '8149189397');
+insert into systemuser(USER_SEQ_NO, USER_NAME, PASSWORD, EMAIL_ID, MOBILE_NO) values(1, 'Sarang Mane', '$2a$10$IsII2xD8FpRREiOLz/bCle5Ape/Eq6EwCyXoPhlbGOULJvZ5Ygh2.', 'sample@email.com', '1234567890');
 
 commit;
